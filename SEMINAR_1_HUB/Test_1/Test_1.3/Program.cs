801cbb31cd18c6f17ee0939b4babe4f551a2987e
Kirill <kirill.uvarov@gmail.com>
2022-09-15 23:09:47 +0300

    Остановился на середине Test_1.3

---
+++ SEMINAR_1_HUB/Test_1/Test_1.3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число 1: ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c;
+c = Convert.ToInt32(Console.ReadLine());
+
+maxNum = 0;
+
+if (a>b)
+    {
+        maxNum=a;
+    }
+else
+    {
+        maxNum=b;
+    }
+Console.WriteLine(maxNum);
