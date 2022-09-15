d8fa3cf12884ff510552456472fab57431535a24
Kirill <kirill.uvarov@gmail.com>
2022-09-15 22:26:01 +0300

    Папки Test_1.1 и Test_1.2

---
+++ SEMINAR_1_HUB/Test_1/Test_1.1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите число: "); // Просим оператора ввести данные
+int num; //Определяем переменную num
+num = Convert.ToInt32(Console.ReadLine()); //Говорим, что num надо конвертировать в название типа простого числа в библиотеке .NET   
+
+    if (num == 1)//Ветвление
+    {
+         Console.WriteLine($"Понедельник");
+    }
+        
+    if (num == 2)
+    {
+         Console.WriteLine($"Вторник");
+    }
+     if (num == 3)
+    {
+         Console.WriteLine($"Среда");
+    }    
+     if (num == 4)
+    {
+         Console.WriteLine($"Четверг");
+    }
+     if (num == 5)
+    {
+         Console.WriteLine($"Пятница");
+    }
+     if (num == 6)
+    {
+         Console.WriteLine($"Суббота");
+    }
+     if (num == 7)
+    {
+         Console.WriteLine($"Воскресение");
+    }
+      if (num > 7)
+    {
+         Console.WriteLine($"Такого дня нет");
+    }
---
+++ SEMINAR_1_HUB/Test_1/Test_1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число 1: ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b;
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    {
+        Console.WriteLine($"Наибольшее число 1:");
+        Console.WriteLine(a);
+        Console.WriteLine($"Наименьшее число 2:");
+        Console.Write(b);
+    }
+else
+    {
+        Console.WriteLine($"Наибольшее число 2:");
+        Console.WriteLine(b);
+        Console.WriteLine($"Наименьшее число 1:");
+        Console.WriteLine(a);
+    }
