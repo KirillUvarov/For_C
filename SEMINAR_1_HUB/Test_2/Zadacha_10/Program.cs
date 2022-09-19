26aea2bb6144d8ffd951d2d2023f5e0843a04637
Kirill <kirill.uvarov@gmail.com>
2022-09-19 13:54:07 +0300

    Выполнил задачу №10

---
+++ SEMINAR_1_HUB/Test_2/Zadacha_10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int x = new Random().Next(1, 10);// Генерация случайного числа
+int y = new Random().Next(1, 10);
+int z = new Random().Next(1, 10);
+Console.Write($"Наше трёхзначное число: {x}");
+Console.Write(y);
+Console.WriteLine(z);
+Console.Write($"Выводим вторую цифру: {y}");
