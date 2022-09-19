6abb1265872c7b48604a9ae2138651edb2c55d1c
Kirill <kirill.uvarov@gmail.com>
2022-09-19 20:28:44 +0300

    Выполнил 13ю задачу

--- SEMINAR_1_HUB/Test_2/Zadacha_13/Program.cs
+++ SEMINAR_1_HUB/Test_2/Zadacha_13/Program.cs
@@ -1,19 +1,21 @@
 ﻿Console.Clear();
-Console.Write("Введите трёхзначное число: "); // Просим оператора ввести данные
+Console.Write("Введите трёх(или больше)-значное  число: "); // Просим оператора ввести данные
 int num;
 num = Convert.ToInt32(Console.ReadLine());
+string numString = Convert.ToString(num);
 
 if (num < 100 & num >= 10)
 {
-    Console.WriteLine("Число двузначное. Третьей цифры нет");
+    Console.WriteLine("Вы ввели двузначное число . Третьего знака нет");
 }
 
 else if ( num < 10)
 {
-    Console.WriteLine("Число однозначное. Второй и третьей цифры нет");
+    Console.WriteLine("Вы ввели однозначное число. Второго и третьего знака нет");
 }
-else if ( num >= 100)
+//else if ( num >= 100)
+
+if (numString.Length > 3) 
 {
-    int ost = num % 10;
-    Console.WriteLine($"Третий знак: {ost}");
+     Console.WriteLine("Третий знак слева: " + numString[2]); 
 }
