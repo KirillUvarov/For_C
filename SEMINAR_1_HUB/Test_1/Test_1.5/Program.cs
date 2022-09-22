d9e6abbd21ba9fd6a0895f937a67cdb00b57c2c0
Kirill <kirill.uvarov@gmail.com>
2022-09-22 12:01:28 +0300

    изменил метод решения Test_1.5

--- SEMINAR_1_HUB/Test_1/Test_1.5/Program.cs
+++ SEMINAR_1_HUB/Test_1/Test_1.5/Program.cs
@@ -10,3 +10,20 @@ int a = 2;
     Console.Write($", ");
     a+=2;
     }
+
+/*
+// Выводит все чётные числа от 1 до n.
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+   if (i % 2 == 0)
+   {
+    Console.Write($"{i} "); 
+    
+   }
+   i++;
+}
+*/
---
+++ SEMINAR_1_HUB/Test_2/Zadacha_13.2/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Write("Введите трёх(или больше)-значное  число: "); // Просим оператора ввести данные
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+int scnd_num = 0;
+
+if (num < 100)
+{
+   Console.Write($"Ваше число {num} не являестя трёхзначным");
+}
+
+else if (num > 100)
+{
+   while (num < 10)
+   {
+      scnd_num = num % 10;
+   }
+   Console.Write($"Третий знак вашего числа { scnd_num }");
+}
+
+/*else if ( num > 999)
+{
+   while (num >= 100)
+   {
+      num = num / 10;
+   }
+
+}
+Console.Write($"Третий знак вашего числа {num}");
+
+
+if (num >= 100 && num <= 1000)
+{
+
+}
+{
+  while (num < 1000)
+   {
+   num = num / 10;
+   Console.WriteLine(num);
+   }
+  
+}
+else
+{
+   Console.Write($"Ваше число {num} не являестя трёхзначным");
+}
+/*/
+
+
+
+
+/*/
+
+
+
+while (num <= 100)
+{
+   num = num % 10;
+   //index++;
+}
+
+Console.WriteLine(num);/*/
