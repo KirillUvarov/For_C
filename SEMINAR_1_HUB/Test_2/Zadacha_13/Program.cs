ef81dc3f2981eaa1c6a4d31bf2e8cfca9efcb4f5
Kirill <kirill.uvarov@gmail.com>
2022-09-25 22:35:24 +0300

    Сделал Zadacha_13

--- SEMINAR_1_HUB/Test_2/Zadacha_13/Program.cs
+++ SEMINAR_1_HUB/Test_2/Zadacha_13/Program.cs
@@ -2,20 +2,38 @@
 Console.Write("Введите трёх(или больше)-значное  число: "); // Просим оператора ввести данные
 int num;
 num = Convert.ToInt32(Console.ReadLine());
-string numString = Convert.ToString(num);
 
-if (num < 100 & num >= 10)
-{
-    Console.WriteLine("Вы ввели двузначное число . Третьего знака нет");
-}
+if (num > 100 )
+    {
+        while (num > 1000)
+        {
+            num = num / 10;
+        }
+    int scnd_num = num % 10;
+    Console.Write($"{scnd_num}");
+    }
+else 
+    {
+        Console.Write($"Ваше число {num} не являестя трёхзначным");
+    }
 
-else if ( num < 10)
-{
-    Console.WriteLine("Вы ввели однозначное число. Второго и третьего знака нет");
-}
-//else if ( num >= 100)
+// int num;
+// num = Convert.ToInt32(Console.ReadLine());
+// string numString = Convert.ToString(num);
 
-if (numString.Length > 3) 
-{
-     Console.WriteLine("Третий знак слева: " + numString[2]); 
-}
+// if (num < 100 & num >= 10)
+// {
+//     Console.WriteLine("Вы ввели двузначное число . Третьего знака нет");
+// }
+
+// else if ( num < 10)
+// {
+//     Console.WriteLine("Вы ввели однозначное число. Второго и третьего знака нет");
+// }
+// //else if ( num >= 100)
+
+// if (numString.Length > 3) 
+// {
+//      Console.WriteLine("Третий знак слева: " + numString[2]); 
+// }
+
