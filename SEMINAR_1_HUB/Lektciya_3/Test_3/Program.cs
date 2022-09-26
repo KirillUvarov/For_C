7ee928c08b168b91020b0d82d4e6c7930add4c2b
Kirill <kirill.uvarov@gmail.com>
2022-09-26 09:44:30 +0300

    Сделал 19, 21, 23 - 2

---
+++ SEMINAR_1_HUB/Lektciya_3/Test_3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Сортировка чисел в массиве
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+   int count = array.Length; 
+
+   for (int i = 0; i < count; i++)
+   {
+    Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+void SelectionSortMinToMax(int[] array)
+{
+    for (int i = 0; i < array.Length-1 ; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSortMaxToMin(int[] array)
+{
+    for (int z = 0; z < array.Length-1 ; z++)
+    {
+        int maxPosition = z;
+        for (int x = z + 1; x < array.Length; x++)
+        {
+            int temporary = array[z];
+            array[z] = array[maxPosition];
+            array[maxPosition] = temporary;
+        }
+
+    }
+}
+
+PrintArray(arr);
+SelectionSortMinToMax(arr);
+PrintArray(arr);
+SelectionSortMaxToMin(arr);
+PrintArray(arr);
